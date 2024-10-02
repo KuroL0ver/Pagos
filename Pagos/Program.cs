8a2cdcd404b60d4090ee09c7229a43b51b1e2bba
Camila Fimbres <camifimbres@gmail.com>
2024-10-02 07:58:19 -0700

    Agregar archivos de proyecto.

---
+++ Pagos/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Pagos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string codigo = DateTime.Now.ToString("yyMMddHHmmss");
+            Console.WriteLine(codigo);
+
+            Console.WriteLine("Introduce tu código de verificación");
+            string codigoEscrito = Console.ReadLine();
+
+      if (codigoEscrito == codigo)
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("codigo incorrecto");
+            }
+        }
+    }
+}
